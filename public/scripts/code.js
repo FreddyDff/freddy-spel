156e381bf7153cf52413b98199842756220272d5
Freddy Fuentes <24freddy.fuentes@glimnet.se>
2026-01-26 10:48:33 +0100

    client events

--- public/scripts/code.js
+++ public/scripts/code.js
@@ -1,1 +1,44 @@
-console.log("hello world");
+// DOM element
+
+const form = document.querySelector('form');
+const msgElement = document.querySelector("input#msg");
+const chatElement = document.querySelector("div#chat");
+
+
+// dependecies - WebSocket
+const websocket = new WebSocket("ws://localhost:8555");
+
+
+
+// variabler, inställningar
+
+
+
+
+
+
+
+
+// händelse lyssnare
+
+// se till att avbryta förvald händelse när knappen skicka klickas
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  console.log("och nu då...");
+});
+
+
+// aktivera lyssnare på input#msg: kan användas för att visa att ngn skriver..
+msgElement.addEventListener("keydown", (e) => {
+  console.log("Ngn skriver", e.key);
+
+
+// hantera att en person skriver ngt - kan kanske skickas som en händelse backend.
+
+});
+
+
+// funktioner
+
+
